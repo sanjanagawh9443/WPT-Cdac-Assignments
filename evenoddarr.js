a5bcb9c292377f1e4a66ca421ca394bef655891a
Sanjana Gawhande <141925971+sanjanagawh9443@users.noreply.github.com>
2026-03-14 14:09:40 +0530

    Add files via upload

---
+++ evenoddarr.js
@@ -0,0 +1,7 @@
+let d=[10,55,5,89,90,24,76,88,34,11] 
+console.log(d);
+let newArray=d.filter((num)=>num%2==0)
+console.log(newArray)
+// sort array data
+let f=d.sort((n1,n2)=>(n1-n1));
+console.log(f);                                                  
