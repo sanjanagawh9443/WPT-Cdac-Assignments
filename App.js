238b394dfc86cd88a3296d98779e204577b95881
Sanjana Gawhande <141925971+sanjanagawh9443@users.noreply.github.com>
2026-03-19 12:22:56 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,46 +1,18 @@
-import React, { useState } from "react";
-
-function isPalindrome(n) {
-  let s = "" + n;  // string
-  let len = s.length;
-
-  for (let i = 0; i < len / 2; i++) {
-    if (s[i] !== s[len - i - 1])
-      return false;
-  }
-  return true;
-}
+import React from 'react';
+import './App.css';
+import SimpleForm from './SimpleForm'; // Apne component ko import kiya
 
 function App() {
-  const [num, setNum] = useState("");
-  const [result, setResult] = useState(null);
-
-  const checkPalindrome = () => {
-    if (num === "") return;
-    setResult(isPalindrome(num));
-  };
-
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Palindrome Checker</h1>
-
-      <input
-        type="number"
-        value={num}
-        onChange={(e) => setNum(e.target.value)}
-        placeholder="Enter a number"
-      />
-
-      <br /><br />
-
-      <button onClick={checkPalindrome}>
-        Check
-      </button>
-
-      <p>
-        {result !== null &&
-          (result ? "Palindrome " : "Not Palindrome ")}
-      </p>
+    <div className="App">
+      <header className="App-header">
+        <h1>React Console Task</h1>
+        <p>Text box mein likhein aur console check karein.</p>
+        
+        {/* Component ko display kar rahe hain */}
+        <SimpleForm />
+        
+      </header>
     </div>
   );
 }
